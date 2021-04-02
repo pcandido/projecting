9ccd51cd4a11e09a22dac86c975a1dfa5d11f611
Paulo Cândido <paulo.candido@g.globo>
2021-04-01 22:27:17 -0300

    test: ensure AddIdeaController calls AddIdeaUseCase with correct params

--- src/controller/add-idea.test.ts
+++ src/controller/add-idea.test.ts
@@ -1,4 +1,21 @@
 import { AddIdeaController } from './add-idea'
+import { AddIdeaUseCase } from '../usecase/add-idea-usecase'
+
+const makeAddIdeaUseCaseStub = () => {
+  class AddIdeaUseCaseStub extends AddIdeaUseCase {
+    addIdea(addIdeaRequest: any): any {
+      return {}
+    }
+  }
+
+  return new AddIdeaUseCaseStub()
+}
+
+const makeSut = () => {
+  const addIdeaUseCaseStub = makeAddIdeaUseCaseStub()
+  const sut = new AddIdeaController(addIdeaUseCaseStub)
+  return { sut, addIdeaUseCaseStub }
+}
 
 describe('AddIdeaController', () => {
 
@@ -11,17 +28,30 @@ describe('AddIdeaController', () => {
     ['title, description', { repository: 'link' }],
     ['repository, description', { title: 'title' }],
   ])('should return 400 if no %s is provided', (expectedMissingParams, givenBody) => {
-    const response = sut().handle(givenBody)
+    const { sut } = makeSut()
+    const response = sut.handle(givenBody)
     expect(response.statusCode).toBe(400)
     expect(response.body).toBe(`Missing param: ${expectedMissingParams}`)
   })
 
   it('should return 200 if success', () => {
-    const response = sut().handle({ repository: 'link', title: 'idea', description: 'short text' })
+    const { sut } = makeSut()
+    const response = sut.handle({ repository: 'link', title: 'idea', description: 'short text' })
     expect(response.statusCode).toBe(200)
   })
 
-  const sut = () => {
-    return new AddIdeaController()
-  }
+  it('should call AddIdeaUseCase with correct params', async () => {
+    const givenBody = {
+      repository: 'link',
+      title: 'idea',
+      description: 'short text',
+    }
+
+    const { sut, addIdeaUseCaseStub } = makeSut()
+    const addIdeaSpy = jest.spyOn(addIdeaUseCaseStub, 'addIdea')
+
+    sut.handle(givenBody)
+
+    expect(addIdeaSpy).toBeCalledWith(givenBody)
+  })
 })
