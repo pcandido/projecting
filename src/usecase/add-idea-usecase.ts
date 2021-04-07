a7ed7435e70a5b5ca6ef288d044686d34c4618d1
rubensolv <rubens_olv@hotmail.com>
2021-04-06 21:58:37 -0300

    feat: change returns in usecase

--- src/usecase/add-idea-usecase.ts
+++ src/usecase/add-idea-usecase.ts
@@ -1,9 +1,9 @@
+import { Idea } from 'src/entity/idea'
 import AddIdeaRequest from '../interface/add-ideia-request-interface'
 export class AddIdeaUseCase {
 
-  addIdea(addIdeaRequest: AddIdeaRequest): any {
-    throw new Error('nem sei o que tem que fazer')
-    return null
+  addIdea(addIdeaRequest: AddIdeaRequest): Idea {
+    throw new Error('nem sei o que tem que fazer')    
   }
 
 }
